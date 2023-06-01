d07cf5a2c4fdea3531045fcdd07cf1580d2af576
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-06-01 19:41:31 -0400

    user controllers

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,20 @@
 import express from "express";
-import {    authUser,
-    logoutUser,
-    registerUser,
-    getUserProfile,
-    updateUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  logoutUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-
-router.post('/auth', authUser);
+// register
+router.post("/", registerUser);
+// login
+router.post("/auth", authUser);
+// logout
+router.post("/logout", logoutUser);
+// get and/or update user profile
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 export default router;
