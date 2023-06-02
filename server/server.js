8a82defd833cbe78dc4091be851d51fdca47e14d
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-06-02 12:36:38 -0400

    database connected

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5001;
 // const port = 5001;
 import userRoutes from "./routes/userRoutes.js";
 
+connectDB();
 const app = express();
 
 // any route using /api/users will be connected to userRoutes
